8aad4feef53addd4ce8ae4d7cd49a2d1aa12f89c
Jashwanth-Gowda-R <jashwanth.go@gmail.com>
2021-11-14 20:06:37 +0530

    added home,cart, account screens

---
+++ grocery_user_app/lib/screens/account.dart
@@ -0,0 +1,16 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class AccountPage extends StatelessWidget {
+  const AccountPage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Account'),
+      ),
+    );
+  }
+}
