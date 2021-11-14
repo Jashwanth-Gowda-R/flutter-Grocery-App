4e8506267c9a1a9441c731a987c6a0dedfe3df72
Jashwanth-Gowda-R <jashwanth.go@gmail.com>
2021-11-14 18:02:03 +0530

    added resized img and main textfor login screen

--- grocery_user_app/lib/screens/login.dart
+++ grocery_user_app/lib/screens/login.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, avoid_unnecessary_containers
 
 import 'package:flutter/material.dart';
 
@@ -12,8 +12,18 @@ class LoginScreen extends StatelessWidget {
       body: Container(
         child: Column(
           children: [
-            Image.asset('assets/images/logo.png'),
-            Text("Shani's Shop"),
+            Image.asset(
+              'assets/images/logo.png',
+              height: 120,
+              width: 120,
+            ),
+            Text(
+              "Shani's Shop",
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
             TextField(),
             TextField(),
             Container(
