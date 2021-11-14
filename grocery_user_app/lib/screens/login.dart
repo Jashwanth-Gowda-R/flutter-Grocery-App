9319a0b86adba645175fe740dba611d1e1543a35
Jashwanth-Gowda-R <jashwanth.go@gmail.com>
2021-11-14 18:22:21 +0530

    added spacing for login screen

--- grocery_user_app/lib/screens/login.dart
+++ grocery_user_app/lib/screens/login.dart
@@ -10,65 +10,104 @@ class LoginScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: Text("Shani's Shop")),
       body: Container(
-        child: Column(
-          children: [
-            Image.asset(
-              'assets/images/logo.png',
-              height: 120,
-              width: 120,
-            ),
-            Text(
-              "Shani's Shop",
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
+        child: Padding(
+          padding: const EdgeInsets.all(32.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/images/logo.png',
+                height: 120,
+                width: 120,
               ),
-            ),
-            TextField(),
-            TextField(),
-            Container(
-              child: ElevatedButton(
-                onPressed: () {},
-                child: Text('Login'),
+              SizedBox(
+                height: 20,
               ),
-            ),
-            Container(
-              child: TextButton(
-                onPressed: () {},
-                child: Text('Create a New Account'),
+              Text(
+                "Shani's Shop",
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-            ),
-            Container(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      primary: Colors.transparent,
-                      elevation: 0,
-                    ),
-                    onPressed: () {},
-                    child: Image.asset(
-                      'assets/images/fb-icon.png',
-                      height: 32,
-                    ),
-                  ),
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      primary: Colors.transparent,
-                      elevation: 0,
-                    ),
-                    onPressed: () {},
-                    child: Image.asset(
-                      'assets/images/google-icon.png',
-                      height: 32,
-                    ),
-                  ),
-                ],
+              SizedBox(
+                height: 20,
               ),
-            )
-          ],
+              TextField(
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.grey[200],
+                  border: InputBorder.none,
+                  labelText: "Email Address",
+                ),
+              ),
+              SizedBox(
+                height: 8,
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.grey[200],
+                  border: InputBorder.none,
+                  labelText: "Password",
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                width: double.infinity,
+                height: 48,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: Text('Login'),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                child: TextButton(
+                  onPressed: () {},
+                  child: Text('Create a New Account'),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.transparent,
+                        elevation: 0,
+                      ),
+                      onPressed: () {},
+                      child: Image.asset(
+                        'assets/images/fb-icon.png',
+                        height: 32,
+                      ),
+                    ),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.transparent,
+                        elevation: 0,
+                      ),
+                      onPressed: () {},
+                      child: Image.asset(
+                        'assets/images/google-icon.png',
+                        height: 32,
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
