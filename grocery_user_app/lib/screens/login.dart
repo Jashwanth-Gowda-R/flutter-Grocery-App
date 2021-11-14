ebf9a59435ceb98da7afbb6a8838180979dd0e66
Jashwanth-Gowda-R <jashwanth.go@gmail.com>
2021-11-14 17:58:53 +0530

    added layout elements for login screen

--- grocery_user_app/lib/screens/login.dart
+++ grocery_user_app/lib/screens/login.dart
@@ -1,12 +1,66 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatelessWidget {
-  const LoginScreen({ Key key }) : super(key: key);
+  const LoginScreen({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      appBar: AppBar(title: Text("Shani's Shop")),
+      body: Container(
+        child: Column(
+          children: [
+            Image.asset('assets/images/logo.png'),
+            Text("Shani's Shop"),
+            TextField(),
+            TextField(),
+            Container(
+              child: ElevatedButton(
+                onPressed: () {},
+                child: Text('Login'),
+              ),
+            ),
+            Container(
+              child: TextButton(
+                onPressed: () {},
+                child: Text('Create a New Account'),
+              ),
+            ),
+            Container(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.transparent,
+                      elevation: 0,
+                    ),
+                    onPressed: () {},
+                    child: Image.asset(
+                      'assets/images/fb-icon.png',
+                      height: 32,
+                    ),
+                  ),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.transparent,
+                      elevation: 0,
+                    ),
+                    onPressed: () {},
+                    child: Image.asset(
+                      'assets/images/google-icon.png',
+                      height: 32,
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
