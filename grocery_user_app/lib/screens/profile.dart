e6cfe730188545ac996620bf0eb434a8428c5c7c
Jashwanth-Gowda-R <jashwanth.go@gmail.com>
2021-11-15 23:03:43 +0530

    added profile page with logout button

--- grocery_user_app/lib/screens/profile.dart
+++ grocery_user_app/lib/screens/profile.dart
@@ -1,10 +1,102 @@
+// ignore_for_file: sized_box_for_whitespace, prefer_const_constructors, avoid_unnecessary_containers
+
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:grocery_user_app/screens/login.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Edit Profile",
+        ),
+      ),
+      body: Container(
+        child: Padding(
+          padding: const EdgeInsets.all(32.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                backgroundImage: AssetImage(
+                  'assets/images/profile.png',
+                ),
+              ),
+              SizedBox(
+                height: 40,
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.grey[200],
+                  border: InputBorder.none,
+                  labelText: "Full Name",
+                ),
+              ),
+              SizedBox(
+                height: 8,
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.grey[200],
+                  border: InputBorder.none,
+                  labelText: "Phone Number",
+                ),
+              ),
+              SizedBox(
+                height: 8,
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.grey[200],
+                  border: InputBorder.none,
+                  labelText: "Email Address",
+                ),
+              ),
+              SizedBox(
+                height: 8,
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.grey[200],
+                  border: InputBorder.none,
+                  labelText: "Password",
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                width: double.infinity,
+                height: 48,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: Text('Save Changes'),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                child: TextButton(
+                  onPressed: () {
+                    Get.offAll(LoginScreen());
+                  },
+                  child: Text('Logout'),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
