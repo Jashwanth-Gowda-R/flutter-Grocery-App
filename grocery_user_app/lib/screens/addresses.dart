e0b91142bdc9324ef0795350b6327d302b974c8a
Jashwanth-Gowda-R <jashwanth.go@gmail.com>
2021-11-17 21:32:43 +0530

    added routes to manage address screen

--- grocery_user_app/lib/screens/addresses.dart
+++ grocery_user_app/lib/screens/addresses.dart
@@ -1,6 +1,8 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:grocery_user_app/screens/manage_address.dart';
 
 class AddressScreen extends StatelessWidget {
   const AddressScreen({Key key}) : super(key: key);
@@ -14,14 +16,20 @@ class AddressScreen extends StatelessWidget {
       body: ListView(
         children: [
           ListTile(
-            title: Text('Home'),
-            subtitle: Text('#4,1st main road,kalyannagar,bengaluru-72'),
-            trailing: Icon(Icons.edit),
-          ),
+              title: Text('Home'),
+              subtitle: Text('#4,1st main road,kalyannagar,bengaluru-72'),
+              trailing: IconButton(
+                onPressed: () {
+                  Get.to(ManageAddress());
+                },
+                icon: Icon(Icons.edit_outlined),
+              )),
         ],
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          Get.to(ManageAddress());
+        },
         child: Icon(Icons.add),
       ),
     );
