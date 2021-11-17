33845ed692698c26cce3d6f93f3238913985f8b9
Jashwanth-Gowda-R <jashwanth.go@gmail.com>
2021-11-17 21:38:23 +0530

    manage address screen finished

--- grocery_user_app/lib/screens/manage_address.dart
+++ grocery_user_app/lib/screens/manage_address.dart
@@ -15,76 +15,82 @@ class ManageAddress extends StatelessWidget {
         child: Column(
           children: [
             Container(
-              child: Image.asset('assets/images/map.png'),
+              child: Image.asset(
+                'assets/images/map.png',
+                fit: BoxFit.cover,
+              ),
+              width: double.infinity,
             ),
             Container(
-              child: Column(
-                children: [
-                  TextField(
-                    decoration: InputDecoration(
-                      filled: true,
-                      fillColor: Colors.grey[200],
-                      border: InputBorder.none,
-                      labelText: "Tag(Home,Office)",
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  children: [
+                    TextField(
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Colors.grey[200],
+                        border: InputBorder.none,
+                        labelText: "Tag(Eg:Home,Office)",
+                      ),
                     ),
-                  ),
-                  SizedBox(
-                    height: 8,
-                  ),
-                  TextField(
-                    decoration: InputDecoration(
-                      filled: true,
-                      fillColor: Colors.grey[200],
-                      border: InputBorder.none,
-                      labelText: "Name",
+                    SizedBox(
+                      height: 8,
                     ),
-                  ),
-                  SizedBox(
-                    height: 8,
-                  ),
-                  TextField(
-                    decoration: InputDecoration(
-                      filled: true,
-                      fillColor: Colors.grey[200],
-                      border: InputBorder.none,
-                      labelText: "Mobile Number",
+                    TextField(
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Colors.grey[200],
+                        border: InputBorder.none,
+                        labelText: "Name",
+                      ),
                     ),
-                  ),
-                  SizedBox(
-                    height: 8,
-                  ),
-                  TextField(
-                    maxLines: 5,
-                    decoration: InputDecoration(
-                      filled: true,
-                      fillColor: Colors.grey[200],
-                      border: InputBorder.none,
-                      labelText: "Address",
+                    SizedBox(
+                      height: 8,
                     ),
-                  ),
-                  SizedBox(
-                    height: 8,
-                  ),
-                  TextField(
-                    decoration: InputDecoration(
-                      filled: true,
-                      fillColor: Colors.grey[200],
-                      border: InputBorder.none,
-                      labelText: "pincode",
+                    TextField(
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Colors.grey[200],
+                        border: InputBorder.none,
+                        labelText: "Mobile Number",
+                      ),
                     ),
-                  ),
-                  SizedBox(
-                    height: 8,
-                  ),
-                  Container(
-                    width: double.infinity,
-                    height: 48,
-                    child: ElevatedButton(
-                      onPressed: () {},
-                      child: Text('Login'),
+                    SizedBox(
+                      height: 8,
                     ),
-                  ),
-                ],
+                    TextField(
+                      maxLines: 5,
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Colors.grey[200],
+                        border: InputBorder.none,
+                        labelText: "Address",
+                      ),
+                    ),
+                    SizedBox(
+                      height: 8,
+                    ),
+                    TextField(
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Colors.grey[200],
+                        border: InputBorder.none,
+                        labelText: "pincode",
+                      ),
+                    ),
+                    SizedBox(
+                      height: 8,
+                    ),
+                    Container(
+                      height: 48,
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        child: Text('Save Changes'),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             )
           ],
