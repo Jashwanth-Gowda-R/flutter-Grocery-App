71cee589bf70ed57d407427d00d34d0f61406ed7
Jashwanth-Gowda-R <jashwanth.go@gmail.com>
2021-11-19 23:15:23 +0530

    added slider in home page

--- grocery_user_app/lib/screens/home.dart
+++ grocery_user_app/lib/screens/home.dart
@@ -1,5 +1,6 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, avoid_unnecessary_containers
 
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -10,6 +11,39 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Home'),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              child: CarouselSlider(
+                options: CarouselOptions(
+                  enableInfiniteScroll: true,
+                  reverse: false,
+                  autoPlay: true,
+                  autoPlayInterval: Duration(seconds: 3),
+                ),
+                items: [
+                  Container(
+                    child: Image.asset('assets/images/offers/1.png'),
+                    padding: EdgeInsets.all(4),
+                  ),
+                  Container(
+                    child: Image.asset('assets/images/offers/2.png'),
+                    padding: EdgeInsets.all(4),
+                  ),
+                  Container(
+                    child: Image.asset('assets/images/offers/3.png'),
+                    padding: EdgeInsets.all(4),
+                  ),
+                ],
+              ),
+            ),
+            Container(),
+            Container(),
+            Container(),
+          ],
+        ),
       ),
     );
   }
