15b5828a81274b0a7593d6a67857d63505ae9437
Jashwanth-Gowda-R <jashwanth.go@gmail.com>
2021-11-22 23:01:06 +0530

    added ui changes for email field

--- grocery_user_app/lib/screens/login.dart
+++ grocery_user_app/lib/screens/login.dart
@@ -14,7 +14,7 @@ class LoginScreen extends StatelessWidget {
   TextEditingController passwordField = TextEditingController();
 
   login() {
-    var email = emailField.text;
+    var email = (emailField.text).trim().toLowerCase();
     var password = passwordField.text;
     _auth
         .signInWithEmailAndPassword(email: email, password: password)
@@ -46,112 +46,115 @@ class LoginScreen extends StatelessWidget {
               color: Colors.green,
             ),
           )),
-      body: Container(
-        child: Padding(
-          padding: const EdgeInsets.all(32.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Image.asset(
-                'assets/images/logo.png',
-                height: 120,
-                width: 120,
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Text(
-                "Shani's Shop",
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
+      body: SingleChildScrollView(
+        child: Container(
+          child: Padding(
+            padding: const EdgeInsets.all(32.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Image.asset(
+                  'assets/images/logo.png',
+                  height: 120,
+                  width: 120,
                 ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              TextField(
-                controller: emailField,
-                decoration: InputDecoration(
-                  filled: true,
-                  fillColor: Colors.grey[200],
-                  border: InputBorder.none,
-                  labelText: "Email Address",
+                SizedBox(
+                  height: 20,
                 ),
-              ),
-              SizedBox(
-                height: 8,
-              ),
-              TextField(
-                controller: passwordField,
-                obscureText: true,
-                decoration: InputDecoration(
-                  filled: true,
-                  fillColor: Colors.grey[200],
-                  border: InputBorder.none,
-                  labelText: "Password",
+                Text(
+                  "Shani's Shop",
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Container(
-                width: double.infinity,
-                height: 48,
-                child: ElevatedButton(
-                  onPressed: () {
-                    login();
-                    // Get.offAll(TabScreen());
-                  },
-                  child: Text('Login'),
+                SizedBox(
+                  height: 20,
                 ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Container(
-                child: TextButton(
-                  onPressed: () {
-                    Get.to(RegisterScreen());
-                  },
-                  child: Text('Create a New Account'),
+                TextField(
+                  controller: emailField,
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.grey[200],
+                    border: InputBorder.none,
+                    labelText: "Email Address",
+                  ),
                 ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Container(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        primary: Colors.transparent,
-                        elevation: 0,
+                SizedBox(
+                  height: 8,
+                ),
+                TextField(
+                  controller: passwordField,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.grey[200],
+                    border: InputBorder.none,
+                    labelText: "Password",
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  width: double.infinity,
+                  height: 48,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      login();
+                      // Get.offAll(TabScreen());
+                    },
+                    child: Text('Login'),
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  child: TextButton(
+                    onPressed: () {
+                      Get.to(RegisterScreen());
+                    },
+                    child: Text('Create a New Account'),
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          primary: Colors.transparent,
+                          elevation: 0,
+                        ),
+                        onPressed: () {},
+                        child: Image.asset(
+                          'assets/images/fb-icon.png',
+                          height: 32,
+                        ),
                       ),
-                      onPressed: () {},
-                      child: Image.asset(
-                        'assets/images/fb-icon.png',
-                        height: 32,
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          primary: Colors.transparent,
+                          elevation: 0,
+                        ),
+                        onPressed: () {},
+                        child: Image.asset(
+                          'assets/images/google-icon.png',
+                          height: 32,
+                        ),
                       ),
-                    ),
-                    ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        primary: Colors.transparent,
-                        elevation: 0,
-                      ),
-                      onPressed: () {},
-                      child: Image.asset(
-                        'assets/images/google-icon.png',
-                        height: 32,
-                      ),
-                    ),
-                  ],
-                ),
-              )
-            ],
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
