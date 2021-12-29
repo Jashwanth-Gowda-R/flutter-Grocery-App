0902f1eddfb1b4eb185cc33a5c6ee0763c4387df
Jashwanth-Gowda-R <jashwanth.go@gmail.com>
2021-12-29 22:45:07 +0530

    added category in product page

--- grocery_admin_app/lib/screens/manage-product.dart
+++ grocery_admin_app/lib/screens/manage-product.dart
@@ -4,71 +4,99 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class ManageProductScreen extends StatelessWidget {
+  List _categories = [
+    "All",
+    "Vegetables",
+    "Meat",
+    "Fruits",
+    "Snacks",
+    "Drinks",
+    "Oils",
+    "Daily Needs"
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Manage Product"),
       ),
-      body: Container(
-        padding: EdgeInsets.all(32.0),
-        width: double.infinity,
-        height: double.infinity,
-        child: Column(
-          children: [
-            CircleAvatar(
-              backgroundImage: AssetImage("assets/images/profile.png"),
-              radius: 60,
-            ),
-            SizedBox(height: 40),
-            TextField(
-              decoration: InputDecoration(
-                filled: true,
-                fillColor: Colors.grey[200],
-                border: InputBorder.none,
-                labelText: "Product Title",
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.all(32.0),
+          child: Column(
+            children: [
+              CircleAvatar(
+                backgroundImage: AssetImage("assets/images/profile.png"),
+                radius: 60,
               ),
-            ),
-            SizedBox(height: 12),
-            TextField(
-              decoration: InputDecoration(
-                filled: true,
-                fillColor: Colors.grey[200],
-                border: InputBorder.none,
-                labelText: "Price",
-              ),
-            ),
-            SizedBox(height: 12),
-            TextField(
-              maxLines: 4,
-              decoration: InputDecoration(
-                filled: true,
-                fillColor: Colors.grey[200],
-                border: InputBorder.none,
-                labelText: "Description",
-              ),
-            ),
-            SizedBox(height: 12),
-            Container(
-              width: double.infinity,
-              height: 48,
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  elevation: 0,
-                  primary: Colors.green,
+              SizedBox(height: 40),
+              TextField(
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.grey[200],
+                  border: InputBorder.none,
+                  labelText: "Product Title",
                 ),
-                child: Text(
-                  "Save Changes",
-                  style: TextStyle(
-                    fontSize: 16,
+              ),
+              SizedBox(height: 12),
+              TextField(
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.grey[200],
+                  border: InputBorder.none,
+                  labelText: "Price",
+                ),
+              ),
+              SizedBox(height: 12),
+              Container(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text('Category'),
+                    DropdownButton(
+                      value: "Oils",
+                      onChanged: (v) {},
+                      items: _categories.map((e) {
+                        return DropdownMenuItem(
+                          child: Text(e),
+                          value: e,
+                        );
+                      }).toList(),
+                    )
+                  ],
+                ),
+              ),
+              TextField(
+                maxLines: 4,
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.grey[200],
+                  border: InputBorder.none,
+                  labelText: "Description",
+                ),
+              ),
+              SizedBox(height: 12),
+              Container(
+                width: double.infinity,
+                height: 48,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    elevation: 0,
+                    primary: Colors.green,
                   ),
+                  child: Text(
+                    "Save Changes",
+                    style: TextStyle(
+                      fontSize: 16,
+                    ),
+                  ),
+                  onPressed: () {
+                    Get.back();
+                  },
                 ),
-                onPressed: () {
-                  Get.back();
-                },
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
