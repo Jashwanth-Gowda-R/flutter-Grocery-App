5e783fd5c4f06841ef20c1b12a0b0f69720a29de
Jashwanth-Gowda-R <jashwanth.go@gmail.com>
2021-12-31 19:50:45 +0530

    added register logic to register screen

--- grocery_user_app/lib/screens/register.dart
+++ grocery_user_app/lib/screens/register.dart
@@ -1,15 +1,55 @@
 // ignore_for_file: prefer_const_constructors, avoid_unnecessary_containers, sized_box_for_whitespace
 
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:grocery_user_app/screens/login.dart';
+import 'package:grocery_user_app/screens/tabs.dart';
 
 class RegisterScreen extends StatelessWidget {
-  const RegisterScreen({Key key}) : super(key: key);
+  RegisterScreen({Key key}) : super(key: key);
+
+  TextEditingController namefield = TextEditingController();
+  TextEditingController emailfield = TextEditingController();
+  TextEditingController passwordfield = TextEditingController();
+  TextEditingController mobilenumberfield = TextEditingController();
+
+  FirebaseAuth _auth = FirebaseAuth.instance;
+  FirebaseFirestore _db = FirebaseFirestore.instance;
+
+  register() {
+    var email = (emailfield.text).trim().toLowerCase();
+    var password = passwordfield.text;
+    _auth
+        .createUserWithEmailAndPassword(email: email, password: password)
+        .then((res) {
+      var userid = res.user.uid;
+      _db
+          .collection('accounts')
+          .doc(userid)
+          .set({
+            "name": namefield.text,
+            "phoneNumber": mobilenumberfield.text,
+            "email": emailfield.text,
+            "createdAt": FieldValue.serverTimestamp(),
+            "imageURL": "http://placehold.it/120x120"
+          })
+          .then((value) => Get.offAll(TabScreen()))
+          .catchError((e) => {print(e)});
+    }).catchError((e) {
+      // print(e);
+      Get.showSnackbar(GetBar(
+        message: e.toString(),
+        duration: Duration(seconds: 5),
+      ));
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: true,
       appBar: AppBar(
         backgroundColor: Colors.white,
         elevation: 0,
@@ -26,125 +66,134 @@ class RegisterScreen extends StatelessWidget {
       body: Container(
         child: Padding(
           padding: const EdgeInsets.all(32.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Image.asset(
-                'assets/images/logo.png',
-                height: 120,
-                width: 120,
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Text(
-                "Shani's Shop",
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Image.asset(
+                  'assets/images/logo.png',
+                  height: 120,
+                  width: 120,
                 ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              TextField(
-                decoration: InputDecoration(
-                  filled: true,
-                  fillColor: Colors.grey[200],
-                  border: InputBorder.none,
-                  labelText: "Full Name",
+                SizedBox(
+                  height: 20,
                 ),
-              ),
-              SizedBox(
-                height: 8,
-              ),
-              TextField(
-                decoration: InputDecoration(
-                  filled: true,
-                  fillColor: Colors.grey[200],
-                  border: InputBorder.none,
-                  labelText: "Phone Number",
+                Text(
+                  "Shani's Shop",
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-              ),
-              SizedBox(
-                height: 8,
-              ),
-              TextField(
-                decoration: InputDecoration(
-                  filled: true,
-                  fillColor: Colors.grey[200],
-                  border: InputBorder.none,
-                  labelText: "Email Address",
+                SizedBox(
+                  height: 20,
                 ),
-              ),
-              SizedBox(
-                height: 8,
-              ),
-              TextField(
-                decoration: InputDecoration(
-                  filled: true,
-                  fillColor: Colors.grey[200],
-                  border: InputBorder.none,
-                  labelText: "Password",
+                TextField(
+                  controller: namefield,
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.grey[200],
+                    border: InputBorder.none,
+                    labelText: "Full Name",
+                  ),
                 ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Container(
-                width: double.infinity,
-                height: 48,
-                child: ElevatedButton(
-                  onPressed: () {},
-                  child: Text('Register'),
+                SizedBox(
+                  height: 8,
                 ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Container(
-                child: TextButton(
-                  onPressed: () {
-                    Get.to(LoginScreen());
-                  },
-                  child: Text('Already Have An Account..?'),
+                TextField(
+                  controller: mobilenumberfield,
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.grey[200],
+                    border: InputBorder.none,
+                    labelText: "Phone Number",
+                  ),
                 ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Container(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        primary: Colors.transparent,
-                        elevation: 0,
+                SizedBox(
+                  height: 8,
+                ),
+                TextField(
+                  controller: emailfield,
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.grey[200],
+                    border: InputBorder.none,
+                    labelText: "Email Address",
+                  ),
+                ),
+                SizedBox(
+                  height: 8,
+                ),
+                TextField(
+                  controller: passwordfield,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.grey[200],
+                    border: InputBorder.none,
+                    labelText: "Password",
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  width: double.infinity,
+                  height: 48,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      register();
+                    },
+                    child: Text('Register'),
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  child: TextButton(
+                    onPressed: () {
+                      Get.to(LoginScreen());
+                    },
+                    child: Text('Already Have An Account..?'),
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          primary: Colors.transparent,
+                          elevation: 0,
+                        ),
+                        onPressed: () {},
+                        child: Image.asset(
+                          'assets/images/fb-icon.png',
+                          height: 32,
+                        ),
                       ),
-                      onPressed: () {},
-                      child: Image.asset(
-                        'assets/images/fb-icon.png',
-                        height: 32,
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          primary: Colors.transparent,
+                          elevation: 0,
+                        ),
+                        onPressed: () {},
+                        child: Image.asset(
+                          'assets/images/google-icon.png',
+                          height: 32,
+                        ),
                       ),
-                    ),
-                    ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        primary: Colors.transparent,
-                        elevation: 0,
-                      ),
-                      onPressed: () {},
-                      child: Image.asset(
-                        'assets/images/google-icon.png',
-                        height: 32,
-                      ),
-                    ),
-                  ],
-                ),
-              )
-            ],
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
