7ea8665ba4b55f227e6bddeeee1dff893aa89243
Jashwanth-Gowda-R <jashwanth.go@gmail.com>
2022-01-01 22:00:35 +0530

    added cart controller

---
+++ grocery_user_app/lib/controller/cart.dart
@@ -0,0 +1,46 @@
+import 'package:get/get.dart';
+
+class CartController extends GetxController {
+  var cart = [].obs;
+
+  addtocart(input) {
+    var index = cart.indexWhere((p) => p['id'] == input['id']);
+    if (index == -1) {
+      input['total'] = input['qty'] * input['price'];
+      cart.add(input);
+    } else {
+      cart[index]['qty'] = cart[index]['qty'] + 1;
+      cart[index]['total'] = cart[index]['total'] + cart[index]['price'];
+    }
+  }
+
+  removefromcart(input) {
+    var index = cart.indexWhere((p) => p['id'] == input['id']);
+    if (index == -1) {
+      print('no present');
+    } else {
+      if (cart[index]['qty'] == 1) {
+        cart.removeAt(index);
+      } else {
+        cart[index]['qty'] = cart[index]['qty'] - 1;
+        cart[index]['total'] = cart[index]['total'] - cart[index]['price'];
+      }
+    }
+  }
+
+  getTotalCartPrice() {
+    num total = 0;
+    cart.forEach((p) {
+      total = total + (p['qty'] + p['price']);
+    });
+    return total;
+  }
+
+  getProductCount() {
+    num count = 0;
+    cart.forEach((p) {
+      count = count + (p['qty'] + p['price']);
+    });
+    return count;
+  }
+}
