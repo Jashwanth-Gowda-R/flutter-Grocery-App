beabacc290a3f28af81cbae504a979436ac11fb3
Jashwanth-Gowda-R <jashwanth.go@gmail.com>
2022-01-02 19:42:25 +0530

    used address controller

--- grocery_user_app/lib/screens/addresses.dart
+++ grocery_user_app/lib/screens/addresses.dart
@@ -2,10 +2,11 @@
 
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:grocery_user_app/controller/address.dart';
 import 'package:grocery_user_app/screens/manage_address.dart';
 
 class AddressScreen extends StatelessWidget {
-  const AddressScreen({Key key}) : super(key: key);
+  AddressController _addressCtrl = AddressController();
 
   @override
   Widget build(BuildContext context) {
@@ -13,22 +14,28 @@ class AddressScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('Addresses'),
       ),
-      body: ListView(
-        children: [
-          ListTile(
-              title: Text('Home'),
-              subtitle: Text('#4,1st main road,kalyannagar,bengaluru-72'),
-              trailing: IconButton(
-                onPressed: () {
-                  Get.to(ManageAddress());
-                },
-                icon: Icon(Icons.edit_outlined),
-              )),
-        ],
-      ),
+      body: Obx(() => ListView.builder(
+          itemCount: _addressCtrl.addresses.length,
+          itemBuilder: (bc, index) {
+            return ListTile(
+                title: Text('${_addressCtrl.addresses[index]['tag']}'),
+                subtitle: Text('${_addressCtrl.addresses[index]['address']}'),
+                trailing: IconButton(
+                  onPressed: () {
+                    Get.to(ManageAddress(
+                      canedit: true,
+                      address: _addressCtrl.addresses[index],
+                    ));
+                  },
+                  icon: Icon(Icons.edit_outlined),
+                ));
+          })),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          Get.to(ManageAddress());
+          Get.to(ManageAddress(
+            canedit: false,
+            address: {},
+          ));
         },
         child: Icon(Icons.add),
       ),
