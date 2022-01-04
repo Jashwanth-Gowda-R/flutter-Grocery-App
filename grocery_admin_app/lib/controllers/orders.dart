41b1f4720ecfdc7697d7c707902b70adb9a246da
Jashwanth-Gowda-R <jashwanth.go@gmail.com>
2022-01-04 12:58:12 +0530

    created date picker and updated the controller

--- grocery_admin_app/lib/controllers/orders.dart
+++ grocery_admin_app/lib/controllers/orders.dart
@@ -10,16 +10,30 @@ class OrderController extends GetxController {
   @override
   onInit() {
     super.onInit();
-    fetchOrders();
+    var today = DateTime.now();
+    fetchOrders(today.toString());
   }
 
-  fetchOrders() {
-    _db.collection('orders').snapshots().listen((res) {
+  fetchOrders(givenDate) {
+    var from = DateTime.parse(givenDate);
+    from = DateTime(from.year, from.month, from.day, 0, 0, 0);
+
+    var to = DateTime.parse(givenDate);
+    to = DateTime(from.year, from.month, from.day, 23, 59, 59);
+
+    _db
+        .collection('orders')
+        .where("createdAt", isGreaterThanOrEqualTo: from)
+        .where("createdAt", isLessThanOrEqualTo: to)
+        .snapshots()
+        .listen((res) {
+      orders.clear();
       var tmp = [];
       res.docs.forEach((order) {
         tmp.add({"id": order.id, ...order.data()});
       });
       orders.assignAll(tmp);
+      // calculate();
     });
   }
 
--- grocery_admin_app/lib/screens/orders.dart
+++ grocery_admin_app/lib/screens/orders.dart
@@ -12,6 +12,7 @@ class OrdersScreen extends StatefulWidget {
 }
 
 class _OrdersScreenState extends State<OrdersScreen> {
+  OrderController _orderCtrl = Get.put(OrderController());
   // List _orderList = [
   //   {
   //     "id": "12345",
@@ -72,8 +73,6 @@ class _OrdersScreenState extends State<OrdersScreen> {
   //   }
   // ];
 
-  OrderController _orderCtrl = Get.put(OrderController());
-
   toDateString(timestamp) {
     var date = DateTime.parse(timestamp.toDate().toString());
     var formatter = DateFormat("dd-MMM-yyyy hh:mm a");
@@ -88,34 +87,47 @@ class _OrdersScreenState extends State<OrdersScreen> {
         actions: [
           IconButton(
             icon: Icon(Icons.date_range),
-            onPressed: () {},
+            onPressed: () async {
+              var picked = await showDatePicker(
+                  context: context,
+                  initialDate: DateTime.now(),
+                  firstDate: DateTime(2021),
+                  lastDate: DateTime.now());
+              if (picked != null) {
+                print(picked);
+                _orderCtrl.fetchOrders(picked.toString());
+              }
+            },
           ),
         ],
       ),
-      body: ListView.builder(
-        itemCount: _orderCtrl.orders.length,
-        itemBuilder: (bc, index) {
-          return ListTile(
-            title: Text("# ${_orderCtrl.orders[index]["id"]}"),
-            subtitle: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text("${toDateString(_orderCtrl.orders[index]["createdAt"])}"),
-                Text(
-                    "${_orderCtrl.orders[index]["itemCount"]} Items - ₹ ${_orderCtrl.orders[index]["cartTotal"]}"),
-              ],
-            ),
-            isThreeLine: true,
-            trailing: Text("${_orderCtrl.orders[index]["status"]}"),
-            onTap: () {
-              Get.to(
-                OrderDetail(
-                  orderObj: _orderCtrl.orders[index],
-                ),
-              );
-            },
-          );
-        },
+      body: Obx(
+        () => ListView.builder(
+          itemCount: _orderCtrl.orders.length,
+          itemBuilder: (bc, index) {
+            return ListTile(
+              title: Text("# ${_orderCtrl.orders[index]["id"]}"),
+              subtitle: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                      "${toDateString(_orderCtrl.orders[index]["createdAt"])}"),
+                  Text(
+                      "${_orderCtrl.orders[index]["itemCount"]} Items - ₹ ${_orderCtrl.orders[index]["cartTotal"]}"),
+                ],
+              ),
+              isThreeLine: true,
+              trailing: Text("${_orderCtrl.orders[index]["status"]}"),
+              onTap: () {
+                Get.to(
+                  OrderDetail(
+                    orderObj: _orderCtrl.orders[index],
+                  ),
+                );
+              },
+            );
+          },
+        ),
       ),
     );
   }
