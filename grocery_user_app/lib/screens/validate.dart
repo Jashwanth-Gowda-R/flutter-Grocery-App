23bb546284deaf9522e34c811db3c5d53c4d8179
Jashwanth-Gowda-R <jashwanth.go@gmail.com>
2022-01-06 15:05:49 +0530

    created auth controller and used for validate screen

--- grocery_user_app/lib/screens/validate.dart
+++ grocery_user_app/lib/screens/validate.dart
@@ -1,8 +1,9 @@
 // ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors, prefer_typing_uninitialized_variables, must_be_immutable, prefer_const_literals_to_create_immutables, avoid_unnecessary_containers, prefer_final_fields
 
-import 'package:firebase_auth/firebase_auth.dart';
 // import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:grocery_user_app/controller/auth.dart';
 // import 'package:grocery_user_app/screens/login.dart';
 import 'package:grocery_user_app/screens/tabs.dart';
 import 'package:grocery_user_app/screens/welcome.dart';
@@ -15,31 +16,35 @@ class Validate extends StatefulWidget {
 }
 
 class _ValidateState extends State<Validate> {
-  FirebaseAuth _auth = FirebaseAuth.instance;
-  bool isloggin = false;
+  // FirebaseAuth _auth = FirebaseAuth.instance;
+  // bool isloggin = false;
 
-  validate() {
-    _auth.authStateChanges().listen((user) {
-      if (user != null) {
-        setState(() {
-          isloggin = true;
-        });
-      } else {
-        setState(() {
-          isloggin = false;
-        });
-      }
-    });
-  }
+  AuthController _auth = Get.put(AuthController());
 
-  @override
-  void initState() {
-    super.initState();
-    validate();
-  }
+  // validate() {
+  //   _auth.authStateChanges().listen((user) {
+  //     if (user != null) {
+  //       setState(() {
+  //         isloggin = true;
+  //       });
+  //     } else {
+  //       setState(() {
+  //         isloggin = false;
+  //       });
+  //     }
+  //   });
+  // }
+
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   validate();
+  // }
 
   @override
   Widget build(BuildContext context) {
-    return isloggin ? TabScreen() : Welcome();
+    return Obx(
+      () => _auth.isloggin.value ? TabScreen() : Welcome(),
+    );
   }
 }
